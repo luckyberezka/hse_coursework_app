4d685a24d51e86962954ec3c4f0e83d5a10920fa
punkmunk <rafa3lka@gmail.com>
2023-02-11 18:47:16 +0300

    implement access rights for different user groups

--- dev/om_hse/__manifest__.py
+++ dev/om_hse/__manifest__.py
@@ -1,10 +1,14 @@
 {
     'name': 'HSE Coursework Management System',
     'author': 'nardzhiev',
+    'category': 'HSE Coursework/Users',
     'depends': ['mail'],
     'data': [
         'views/menu.xml',
         'views/coursework.xml',
         'security/ir.model.access.csv',
-    ]
+        'security/security.xml',
+    ],
+    'application': True,
+    'installable': True,
 }
--- dev/om_hse/models/coursework.py
+++ dev/om_hse/models/coursework.py
@@ -6,50 +6,51 @@ class HseCoursework(models.Model):
     _inherit = 'mail.thread'
     _description = 'record with info about new hse coursework'
 
-    ru_title = fields.Char(string='Title(ru)', required=True, tracking=True)
+    is_approved = fields.Boolean(groups='om_hse.group_hse_manager')
     en_title = fields.Char(string='Title(en)', required=True, tracking=True)
-    description = fields.Text(string='Description')
-    education_level = fields.Char(string='Education level', required=True, tracking=True)
-    branch = fields.Selection(
-        [('moscow', 'Moscow'), ('st. petersburg', 'St. Petersburg'), ('nizhny novgorod', 'Nizhny Novgorod')],
-        string='Branch',
-        tracking=True,
-        required=True,
-    )
-    faculty = fields.Selection(
-        [('computer science faculty', 'Computer Science Faculty')],
-        string='Faculty',
-        required=True,
-    )
-    educational_program = fields.Selection(
-        [
-            ('applied mathematics and informatics', 'Applied Mathematics and Informatics'),
-            ('software engineering', 'Software Engineering'),
-            ('applied data analysis', 'Applied Data Analysis'),
-            ('computer science and data analysis', 'Computer Science and Data Analysis'),
-            ('economics and data analysis', 'Economics and Data Analysis'),
+    # ru_title = fields.Char(string='Title(ru)', required=True, tracking=True)
+    # description = fields.Text(string='Description')
+    # education_level = fields.Char(string='Education level', required=True, tracking=True)
+    # branch = fields.Selection(
+    #     [('moscow', 'Moscow'), ('st. petersburg', 'St. Petersburg'), ('nizhny novgorod', 'Nizhny Novgorod')],
+    #     string='Branch',
+    #     tracking=True,
+    #     required=True,
+    # )
+    # faculty = fields.Selection(
+    #     [('computer science faculty', 'Computer Science Faculty')],
+    #     string='Faculty',
+    #     required=True,
+    # )
+    # educational_program = fields.Selection(
+    #     [
+    #         ('applied mathematics and informatics', 'Applied Mathematics and Informatics'),
+    #         ('software engineering', 'Software Engineering'),
+    #         ('applied data analysis', 'Applied Data Analysis'),
+    #         ('computer science and data analysis', 'Computer Science and Data Analysis'),
+    #         ('economics and data analysis', 'Economics and Data Analysis'),
 
-        ],
-        string='Educational program',
-        required=True,
-    )
+    #     ],
+    #     string='Educational program',
+    #     required=True,
+    # )
 
-    language = fields.Selection(
-        [('russian', 'Russian'), ('english', 'English')],
-        string='Language',
-        required=True,
-    )
-    coursework_type = fields.Selection(
-        [('student\'s graduate thesis', 'Student\'s Graduate Thesis'), ('coursework', 'Coursework')],
-        string='Coursework type',
-        required=True,
-    )
-    coursework_format = fields.Selection(
-        [('academic', 'Academic'), ('project', 'Project'), ('startup', 'Startup')],
-        string='Coursework format',
-        required=True,
-    )
-    course = fields.Integer(string='Course', required=True)
+    # language = fields.Selection(
+    #     [('russian', 'Russian'), ('english', 'English')],
+    #     string='Language',
+    #     required=True,
+    # )
+    # coursework_type = fields.Selection(
+    #     [('student\'s graduate thesis', 'Student\'s Graduate Thesis'), ('coursework', 'Coursework')],
+    #     string='Coursework type',
+    #     required=True,
+    # )
+    # coursework_format = fields.Selection(
+    #     [('academic', 'Academic'), ('project', 'Project'), ('startup', 'Startup')],
+    #     string='Coursework format',
+    #     required=True,
+    # )
+    # course = fields.Integer(string='Course', required=True)
 
     # @api.onchange('faculty')
     # def _onchange_faculty(self):
