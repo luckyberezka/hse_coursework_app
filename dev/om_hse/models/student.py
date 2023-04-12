50f704bd8b41927c1b40d406b7fe8a3c224fea56
nardzhiev <nanardzhiev@edu.hse.ru>
2023-04-12 10:41:47 +0300

    student apply view updated

--- dev/om_hse/models/student.py
+++ dev/om_hse/models/student.py
@@ -6,4 +6,40 @@ class HseStudent(models.Model):
     _inherit = 'mail.thread'
     _description = 'Student Apply'
 
-    student_name = fields.Char(string='Name', required=True, tracking=True)
+    student_name = fields.Char(string='Name', readonly=True)
+    course = fields.Integer(string='Course', readonly=True)
+    educational_program = fields.Selection(
+        [
+            ('applied mathematics and informatics', 'Applied Mathematics and Informatics'),
+            ('software engineering', 'Software Engineering'),
+            ('applied data analysis', 'Applied Data Analysis'),
+            ('computer science and data analysis', 'Computer Science and Data Analysis'),
+            ('economics and data analysis', 'Economics and Data Analysis'),
+
+        ],
+        string='Educational program',
+        readonly=True,
+    )
+
+    state = fields.Selection(
+        [
+            ('under_consideration', 'Under Consideration'),
+            ('rejected', 'Rejected'),
+            ('accepted', 'Accepted'),
+        ],
+        default='under_consideration',
+        string='Verdict',
+        readonly=True,
+    )
+
+    program = fields.Char(string='Program', readonly=True)
+
+    title = fields.Char(string='Title', readonly=True)
+
+    def action_accepted(self):
+        for rec in self:
+            rec.state = 'accepted'
+
+    def action_rejected(self):
+        for rec in self:
+            rec.state = 'rejected'
--- dev/om_hse/wizard/apply_coursework.py
+++ dev/om_hse/wizard/apply_coursework.py
@@ -6,7 +6,7 @@ class ApplyCourseworkWizard(models.TransientModel):
     _description = "Apply Coursework Wizard"
 
     coursework_id = fields.Many2one(comodel_name='hse.coursework', string="ID")
-    reason = fields.Text(string="Reason")
+    comment = fields.Text(string="Comment")
 
     @api.model
     def default_get(self, fields):
@@ -15,7 +15,37 @@ class ApplyCourseworkWizard(models.TransientModel):
         return res
 
     def action_apply(self):
+        user_pool = self.env['res.users']
+        user = user_pool.browse(self._uid)
+
+        course = 3
+        educational_program = 'Applied Mathematics and Informatics'
+
+        # ami_str_pref = 'hse.coursework.student_ami'
+        #
+        # for i in range(1, 5):
+        #     current_ami_str = ami_str_pref + str(i)
+        #     if user.has_group(current_ami_str):
+        #         course = i
+        #         educational_program = 'Applied Mathematics and Informatics'
+        #         break
+        #
+        # se_str_pref = 'hse.coursework.student_se'
+        #
+        # for i in range(1, 5):
+        #     current_se_str = se_str_pref + str(i)
+        #     if user.has_group(current_se_str):
+        #         course = i
+        #         educational_program = 'Software Engineering'
+        #         break
+
+        leads = self.env['hse.coursework'].browse(self.env.context.get('active_ids'))
+
         vals = {
-            'student_name': (self.env['res.users'].search([('name', '=', self.env.user.name)])).name
+            'student_name': (self.env['res.users'].search([('name', '=', self.env.user.name)])).name,
+            'course': course,
+            'program': educational_program,
+            'title': leads.en_title,
         }
+        
         self.env['hse.student'].create(vals)
